7971079c6055507f0e67cac9e04fc1acd715667b
Santiago <nevadosantiago97@gmail.com>
2020-07-15 11:25:37 -0300

    detalle sucursal dto y daoImpl

--- turnero-commons/src/main/java/com/cda/turnero/dto/DetalleSucursalDto.java
+++ turnero-commons/src/main/java/com/cda/turnero/dto/DetalleSucursalDto.java
@@ -1,26 +1,51 @@
 package com.cda.turnero.dto;
 
+import com.cda.turnero.model.ConfiguracionSucursal;
+
 public class DetalleSucursalDto {
 
 	private Integer sucursalId;
-	private String nombreSucursal;
+	private String nombre;
 	private String direccion;
 	private Long cantidadPersonas;
-	private String latitud;
-	private String longitud;
+	private ConfiguracionSucursal configuracion;
 	
 	public DetalleSucursalDto() {
 		
 	}
-	public DetalleSucursalDto(int sucursalId, String nombreSucursal, String direccion, Long cantidadPersona) {
-		this.nombreSucursal = nombreSucursal;
+	public DetalleSucursalDto(int sucursalId, String nombreSucursal, String direccion, Long cantidadPersona, String cordLongitud, String cordLatitud) {
+		this.nombre = nombreSucursal;
 		this.direccion= direccion;
 		this.cantidadPersonas = cantidadPersona;
 		this.sucursalId = sucursalId;
+		ConfiguracionSucursal cs = new ConfiguracionSucursal();
+		cs.setCordLatitud(cordLatitud);
+		cs.setCordLongitud(cordLongitud);
+		this.configuracion = cs;
+		
+	}
+	public DetalleSucursalDto(int sucursalId, String nombreSucursal, String direccion,String cordLongitud, String cordLatitud) {
+		this.nombre = nombreSucursal;
+		this.direccion= direccion;
+		this.cantidadPersonas = 0L;
+		this.sucursalId = sucursalId;
+		ConfiguracionSucursal cs = new ConfiguracionSucursal();
+		cs.setCordLatitud(cordLatitud);
+		cs.setCordLongitud(cordLongitud);
+		this.configuracion = cs;
+		
 	}
 	
+	
+	
+	public ConfiguracionSucursal getConfiguracion() {
+		return configuracion;
+	}
+	public void setConfiguracion(ConfiguracionSucursal configuracion) {
+		this.configuracion = configuracion;
+	}
 	public String getNombreSucursal() {
-		return nombreSucursal;
+		return nombre;
 	}
 	public Integer getSucursalId() {
 		return sucursalId;
@@ -29,7 +54,7 @@ public class DetalleSucursalDto {
 		this.sucursalId = sucursalId;
 	}
 	public void setNombreSucursal(String nombreSucursal) {
-		this.nombreSucursal = nombreSucursal;
+		this.nombre = nombreSucursal;
 	}
 	public String getDireccion() {
 		return direccion;
@@ -43,18 +68,13 @@ public class DetalleSucursalDto {
 	public void setCantidadPersonas(Long cantidadPersonas) {
 		this.cantidadPersonas = cantidadPersonas;
 	}
-	public String getLatitud() {
-		return latitud;
+	public String getNombre() {
+		return nombre;
 	}
-	public void setLatitud(String latitud) {
-		this.latitud = latitud;
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
 	}
-	public String getLongitud() {
-		return longitud;
-	}
-	public void setLongitud(String longitud) {
-		this.longitud = longitud;
-	}
+	
 	
 	
 	
--- turnerodao/src/main/java/com/cda/turnero/dao/SucursalDaoImpl.java
+++ turnerodao/src/main/java/com/cda/turnero/dao/SucursalDaoImpl.java
@@ -7,6 +7,7 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
 import javax.persistence.criteria.Root;
 import javax.persistence.criteria.Subquery;
 
@@ -22,35 +23,53 @@ public class SucursalDaoImpl {
 	@PersistenceContext	
 	private EntityManager entityManager;
 
-
-//	public List<Object[]> getSucursalesByReservas() {
-//		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-//		
-//		CriteriaQuery<Object[]> query = criteriaBuilder.createQuery(Object[].class);
-//		Root<Reserva> reserva = query.from(Reserva.class);
-//		
-//		query.where(criteriaBuilder.isNull(reserva.get("fechaSalida")));
-//		Subquery querySucursal = query.subquery(Sucursal.class);
-//		Root<Sucursal> sucursal = querySucursal.from(Sucursal.class);
-//		querySucursal.groupBy(sucursal.get("sucursalId"));
-//		query.multiselect(sucursal.get("sucursalId"), sucursal.get("nombre"), sucursal.get("direccion"), criteriaBuilder.count(sucursal.get("sucursalId")));
-//		query.orderBy(criteriaBuilder.desc(criteriaBuilder.count(sucursal.get("sucursalId"))));
-//		TypedQuery<Object[]> typedQuery = entityManager.createQuery(query);
-//		List<Object[]> resultList = typedQuery.getResultList();
-//		return resultList;
-//	}
 	public List<DetalleSucursalDto> getSucursalesByReservas() {
 		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
 		
 		CriteriaQuery<DetalleSucursalDto> query = criteriaBuilder.createQuery(DetalleSucursalDto.class);
 		Root<Reserva> reserva = query.from(Reserva.class);
+		Join sucursal  = reserva.join("sucursal");
 		query.where(criteriaBuilder.isNull(reserva.get("fechaSalida")));
-		query.groupBy(reserva.join("sucursal").get("sucursalId"));
-		query.multiselect(reserva.join("sucursal").get("sucursalId"), reserva.join("sucursal").get("nombre"), reserva.join("sucursal").get("direccion"), criteriaBuilder.count(reserva.join("sucursal").get("sucursalId")));
-		query.orderBy(criteriaBuilder.asc(criteriaBuilder.count(reserva.join("sucursal").get("sucursalId"))));
+		query.multiselect(sucursal.get("sucursalId"), sucursal.get("nombre"), sucursal.get("direccion"), criteriaBuilder.count(sucursal.get("sucursalId")),sucursal.join("configuracion").get("cordLongitud"),sucursal.join("configuracion").get("cordLatitud"));
+		query.groupBy(sucursal.get("sucursalId"));
+		query.orderBy(criteriaBuilder.asc(criteriaBuilder.count(sucursal.get("sucursalId"))));
+		TypedQuery<DetalleSucursalDto> typedQuery = entityManager.createQuery(query);
+		List<DetalleSucursalDto> resultList = typedQuery.getResultList();
+		List<DetalleSucursalDto> resultado = getSucursalesSinReservas();
+		resultado.addAll(resultList);
+		return resultado;
+	}
+	private List<DetalleSucursalDto> getSucursalesSinReservas(){
+		
+		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();		
+		CriteriaQuery<DetalleSucursalDto> query = criteriaBuilder.createQuery(DetalleSucursalDto.class);
+		Subquery<Reserva> reservaSubquery = query.subquery(Reserva.class);
+		Root<Reserva> reserva = reservaSubquery.from(Reserva.class);
+		Root<Sucursal> sucursal = query.from(Sucursal.class);
+		reservaSubquery.select(reserva.join("sucursal").get("sucursalId"))
+		  .where(criteriaBuilder.isNull(reserva.get("fechaSalida")));
+		query.multiselect(sucursal.get("sucursalId"), sucursal.get("nombre"), sucursal.get("direccion"), sucursal.join("configuracion").get("cordLongitud"),sucursal.join("configuracion").get("cordLatitud"))
+		  .where(criteriaBuilder.not(criteriaBuilder.in(sucursal.get("sucursalId")).value(reservaSubquery)));
 		TypedQuery<DetalleSucursalDto> typedQuery = entityManager.createQuery(query);
 		List<DetalleSucursalDto> resultList = typedQuery.getResultList();
 		return resultList;
+		
 	}
-	
+//	public List<DetalleSucursalDto> getSucursalesByReservas() {
+//		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+//		
+//		CriteriaQuery<DetalleSucursalDto> query = criteriaBuilder.createQuery(DetalleSucursalDto.class);
+//		CriteriaQuery<Reserva> queryReserva = criteriaBuilder.createQuery(Reserva.class);
+//		
+//		Root<Sucursal> sucursal = query.from(Sucursal.class);
+//		Subquery<Long> countReserva = queryReserva.subquery(Long.class);
+//		query.where(criteriaBuilder.isNull(reserva.get("fechaSalida")));
+//		countReserva.select(criteriaBuilder.count(sucursal.get("sucursalId")));
+//		query.multiselect(sucursal.get("sucursalId"), sucursal.get("nombre"), sucursal.get("direccion"), criteriaBuilder.count(sucursal.get("sucursalId")));
+//		query.groupBy(sucursal.get("sucursalId"));
+//		query.orderBy(criteriaBuilder.desc(criteriaBuilder.count(sucursal.get("sucursalId"))));
+//		TypedQuery<DetalleSucursalDto> typedQuery = entityManager.createQuery(query);
+//		List<DetalleSucursalDto> resultList = typedQuery.getResultList();
+//		return resultList;
+//	}
 }
