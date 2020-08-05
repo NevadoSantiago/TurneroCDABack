12af91e70719fd5ce1ddb72ff260d9a8a8d04527
Santiago <nevadosantiago97@gmail.com>
2020-08-05 13:36:27 -0300

    No token para validar

--- usuarioapi/src/main/java/com/cda/turnero/config/WebSecurityConfig.java
+++ usuarioapi/src/main/java/com/cda/turnero/config/WebSecurityConfig.java
@@ -69,7 +69,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 // We don't need CSRF for this example
 		httpSecurity.csrf().disable()
 // dont authenticate this particular request
-				.authorizeRequests().antMatchers("/api/usuario/auth").permitAll().
+				.authorizeRequests().antMatchers("/api/usuario/auth","/api/usuario/validarCodigo/*").permitAll().
 // all other requests need to be authenticated
 				anyRequest().authenticated().and().
 // make sure we use stateless session; session won't be used to
